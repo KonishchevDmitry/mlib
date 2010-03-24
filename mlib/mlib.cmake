815c5ef2794f7fa8b6869d5514b692855f8190b9
Dmitry Konishchev <konishchev@gmail.com>
2010-03-24 20:41:01 +0300

    [+] qmake changed to CMake

---
+++ mlib/mlib.cmake
@@ -0,0 +1,64 @@
+# TODO boost
+
+# Configures MLib.
+#
+# Application is not allowed to configure Qt by itself (CMake's Qt module can't
+# be called twice).
+#
+# Application must set:
+#  MLIB_APP_MLIB_PARENT_DIR - directory in which MLib located.
+#
+# Application may set:
+#  MLIB_APP_QT_MODULES - Qt modules that application use.
+#  MLIB_DEBUG_MODE - enables debug mode.
+#  MLIB_DEVELOP_MODE - enables develop mode.
+#  MLIB_ENABLE_ALIASES - enables global namespace aliases for common classes
+#      and functions.
+
+
+find_package(Qt4 COMPONENTS QtCore ${MLIB_APP_QT_MODULES} REQUIRED)
+include(${QT_USE_FILE})
+set(MLIB_LIBRARIES ${MLIB_LIBRARIES} ${QT_LIBRARIES})
+
+
+# Defines -->
+	add_definitions(-DMLIB_ENABLE_ALIASES)
+
+	if(MLIB_DEBUG_MODE)
+		add_definitions(-DMLIB_DEBUG_MODE)
+	endif()
+
+	if(MLIB_DEVELOP_MODE)
+		add_definitions(-DMLIB_DEVELOP_MODE)
+	endif()
+# Defines <--
+
+
+# Includes
+include_directories(${MLIB_APP_MLIB_PARENT_DIR})
+
+
+# Compiler CFLAGS -->
+	# Large files support
+	if(UNIX)
+		add_definitions(-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64)
+	endif()
+
+	if(CMAKE_COMPILER_IS_GNUCXX)
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+
+		# Some of this options are not supported by older versions of GCC
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fdiagnostics-show-option")
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Winit-self -Wpointer-arith -Wlogical-op -Wcast-qual -Wcast-align -Wnon-virtual-dtor")
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Werror=reorder -Werror=main -Werror=missing-braces -Werror=array-bounds -Werror=sequence-point -Werror=return-type")
+
+		# GCC forgets about -Wno-* options when -O* flag is specified in CXXFLAGS
+		set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wextra -Wno-unused-parameter -Wno-ignored-qualifiers -Wno-empty-body")
+
+		#-Winline -Weffc++ -Wold-style-cast -Wsign-conversion -Wconversion -Wfloat-equal -Wstack-protector -Woverloaded-virtual
+	endif()
+# Compiler CFLAGS <--
+
+
+link_directories(${MLIB_APP_MLIB_PARENT_DIR}/mlib)
+set(MLIB_LIBRARIES ${MLIB_LIBRARIES} mlib)
